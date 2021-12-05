cc605d6b5e10bbb3d0c63053c232d367bb7710d6
Roberto <anacleto44@gmail.com>
2021-12-05 19:11:38 +0100

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace UPSTray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.chargingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.voltageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.powerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timeRemainingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(18, 18);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.powerToolStripMenuItem,
+            this.timeRemainingToolStripMenuItem,
+            this.voltageToolStripMenuItem,
+            this.chargingToolStripMenuItem,
+            this.toolStripMenuItem3});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.ShowImageMargin = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(161, 124);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // chargingToolStripMenuItem
+            // 
+            this.chargingToolStripMenuItem.Name = "chargingToolStripMenuItem";
+            this.chargingToolStripMenuItem.Size = new System.Drawing.Size(167, 24);
+            this.chargingToolStripMenuItem.Text = "Charging";
+            // 
+            // voltageToolStripMenuItem
+            // 
+            this.voltageToolStripMenuItem.Name = "voltageToolStripMenuItem";
+            this.voltageToolStripMenuItem.Size = new System.Drawing.Size(167, 24);
+            this.voltageToolStripMenuItem.Text = "Voltage";
+            // 
+            // powerToolStripMenuItem
+            // 
+            this.powerToolStripMenuItem.Name = "powerToolStripMenuItem";
+            this.powerToolStripMenuItem.Size = new System.Drawing.Size(167, 24);
+            this.powerToolStripMenuItem.Text = "Power";
+            // 
+            // timeRemainingToolStripMenuItem
+            // 
+            this.timeRemainingToolStripMenuItem.Name = "timeRemainingToolStripMenuItem";
+            this.timeRemainingToolStripMenuItem.Size = new System.Drawing.Size(167, 24);
+            this.timeRemainingToolStripMenuItem.Text = "Time remaining: -";
+            this.timeRemainingToolStripMenuItem.Click += new System.EventHandler(this.timeRemainingToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(167, 24);
+            this.toolStripMenuItem3.Text = "100%";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem chargingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem voltageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem powerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem timeRemainingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Serialization.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UPSTray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Visible = false; // Hide form window.
+            ShowInTaskbar = false; // Remove from taskbar.
+            Opacity = 0;
+
+            Login();
+
+            if (bearer != null)
+            {
+                timer1.Interval = 5000;
+                timer1.Start();
+                timer1_Tick(sender, e);
+            }
+        }
+
+        String bearer = null;
+
+        void Login()
+        {
+            var client = new RestClient("http://localhost:3052/local/rest/v1/login/verify");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            var body = @"{""userName"":""admin"",""password"":""admin""}";
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            var x = response.Content.Split(' ');
+            
+            if (x[0].Trim('"') == "Bearer")
+            {
+                bearer = x[1].Trim('"');
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var client = new RestClient("http://localhost:3052/local/rest/v1/ups/status");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Authorization", "Bearer "+bearer);            
+            IRestResponse response = client.Execute(request);
+
+            var obj = JsonConvert.DeserializeObject<dynamic>(response.Content);
+
+            NumberFormatInfo provider = new NumberFormatInfo();
+            provider.NumberDecimalSeparator = ".";
+            provider.NumberGroupSeparator = "";
+            provider.NumberGroupSizes = new int[] { 3 };
+
+
+            String battery_voltage = obj["battery"]["voltage"];
+            double b_volt = Convert.ToDouble(battery_voltage.Split(' ')[0], provider);
+            String load = obj["output"]["loads"][0];
+            double power_perc = Convert.ToDouble(load.Split(' ')[0], provider);
+            int battery_state = obj["battery"]["state"];
+            int input_state = obj["input"]["state"];
+            String input_voltage = obj["input"]["voltages"][0];
+            double i_volt = Convert.ToDouble(input_voltage.Split(' ')[0], provider);
+
+            if (input_state == 1) //blackout
+            {
+                
+                b_volt -= 6.1;
+                if (b_volt >= 12.3)
+                {
+                    notifyIcon1.Icon = Properties.Resources.battery_full;
+                } else if (b_volt >= 12.2)
+                {
+                    notifyIcon1.Icon = Properties.Resources.battery_half;
+                }
+                else if (b_volt >= 11.9)
+                {
+                    notifyIcon1.Icon = Properties.Resources.battery_third;
+                }
+                else
+                {
+                    notifyIcon1.Icon = Properties.Resources.battery_empty;
+                }
+
+                contextMenuStrip1.Items[3].Text = "Status: Blackout";
+            } else // normal
+            {
+                b_volt -= 6.6;
+                notifyIcon1.Icon = Properties.Resources.battery_charge;
+                contextMenuStrip1.Items[3].Text = "Status: Charging";
+            }
+
+
+            contextMenuStrip1.Items[2].Text = "Battery: " + b_volt + "V";
+            contextMenuStrip1.Items[0].Text = "Load: " + (480 * power_perc / 100) + "W (480W)";
+            contextMenuStrip1.Items[4].Text = "Charge: 100%";
+        }
+
+        private void timeRemainingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
